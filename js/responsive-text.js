90f8b84375ae294b47c75300ee91f9398cc5d4e8
“Valerie <valerievarias@gmail.com>
2015-11-17 11:21:18 +0100

    imported files

---
+++ js/responsive-text.js
@@ -0,0 +1,17 @@
+$(function() {
+
+	$('.intro h1').flowtype({
+		minFont   : 30,
+		fontRatio : 9,
+	});	
+	$('.intro h2').flowtype({
+		minFont   : 24,
+		fontRatio : 40,
+	});
+	
+	$('.second-text h1').flowtype({
+		minFont   : 22,
+		fontRatio : 38
+	});
+	
+});	
